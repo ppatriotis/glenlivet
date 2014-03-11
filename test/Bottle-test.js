f2c96fe3af61cda8eee32f7bfe479ee75766a7c4
Eric Weber <eric@prolificinteractive.com>
2014-03-11 15:11:48 -0400

    Add convenience methods for triggering hooks on bottles

--- test/Bottle-test.js
+++ test/Bottle-test.js
@@ -15,6 +15,85 @@ describe('Glenlivet bottles', function () {
 		});
 	});
 
+	it('should create helper methods for each top-level hook', function (done) {
+		var barrel = glenlivet.createBarrel();
+
+		barrel.plugins.register(function testPlugin (context) {
+			context.is(glenlivet.Bottle, function (bottle, myConfig) {
+				bottle.hooks.add({
+					testPlugin: {}
+				});
+
+				bottle.hooks.after('testPlugin', function () {
+					done();
+				});
+			});
+		});
+
+		var testBottle = barrel.createBottle('testBottle', {
+			testPlugin: {}
+		});
+
+		testBottle.testPlugin({
+			foo: ''
+		}, function () {});
+	});
+
+	it('should create a namespace on hook result objects for each top-level hook', function (done) {
+		var barrel = glenlivet.createBarrel();
+
+		barrel.plugins.register(function testPlugin (context) {
+			context.is(glenlivet.Bottle, function (bottle, myConfig) {
+				bottle.hooks.add({
+					testPlugin: {}
+				});
+
+				bottle.hooks.after('testPlugin', function (result) {
+					result.testPlugin.foo = 'bar'; //Expects testPlugin to be there
+				});
+			});
+		});
+
+		var testBottle = barrel.createBottle('testBottle', {
+			testPlugin: {}
+		});
+
+		testBottle.testPlugin({
+			testPlugin: {
+				foo: ''
+			}
+		}, function (result) {
+			result.testPlugin.foo.should.equal('bar');
+			done();
+		});
+	});
+
+	it('should create a namespace on hook result objects but not override existing ones', function (done) {
+		var barrel = glenlivet.createBarrel();
+
+		barrel.plugins.register(function testPlugin (context) {
+			context.is(glenlivet.Bottle, function (bottle, myConfig) {
+				bottle.hooks.add({
+					testPlugin: {}
+				});
+			});
+		});
+
+		var testBottle = barrel.createBottle('testBottle', {
+			testPlugin: {}
+		});
+
+		testBottle.testPlugin({
+			testPlugin: {
+				foo: '',
+				x: 1
+			}
+		}, function (result) {
+			result.testPlugin.x.should.equal(1);
+			done();
+		});
+	});
+
 	it('should resolve plugins registered on the parent Barrel object', function () {
 		var barrel = glenlivet.createBarrel({});
 
