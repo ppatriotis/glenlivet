bf70dd6c1c32539ed01b77b7cfe24d3adebdc1d0
Eric Weber <eric@prolificinteractive.com>
2014-03-12 12:18:33 -0400

    Allow plugin context to be identified by constructor function or name string

--- test/PluginContext-test.js
+++ test/PluginContext-test.js
@@ -1,6 +1,48 @@
 var PluginContext = require('../index').PluginContext;
 
 describe('Running plugins within a context', function () {
+	it('should be able to match context type by constructor function', function () {
+		var instance = new Class();
+		var context = new PluginContext(instance, xPlugin);
+
+		xPlugin.call(instance, context);
+
+		function Class () {
+			this.pluginConfig = {
+				xPlugin: {
+					x: 5
+				}
+			};
+		}
+
+		function xPlugin (context) {
+			context.is(Class, function (instance, xConfig) {
+				xConfig.x.should.equal(5);
+			});
+		}
+	});
+
+	it('should be able to match context type by constructor name', function () {
+		var instance = new Class();
+		var context = new PluginContext(instance, xPlugin);
+
+		xPlugin.call(instance, context);
+		
+		function Class () {
+			this.pluginConfig = {
+				xPlugin: {
+					x: 5
+				}
+			};
+		}
+
+		function xPlugin (context) {
+			context.is('Class', function (instance, xConfig) {
+				xConfig.x.should.equal(5);
+			});
+		}
+	});
+
 	it('should be able to combine with other plugins in that context', function () {
 		function Class () {
 			this.pluginConfig = {
